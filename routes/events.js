71a7a26d113fedfef08fb33d5774274bb90e8bf4
Bruno Farias <brunofarias1@gmail.com>
2018-01-22 15:02:44 -0200

    teste cors

--- routes/events.js
+++ routes/events.js
@@ -14,9 +14,9 @@ router.all('/', function(req, res, next) {
         }
     ]
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(result));
 });
